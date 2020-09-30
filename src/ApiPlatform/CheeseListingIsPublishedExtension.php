6f04447214fba5903b7cb88f13b97b7b09d9b9f3
Aymeric <hello@codeparadise.xyz>
2020-09-30 20:04:26 +0200

    Automatic 404 on Unpublished Items

--- src/ApiPlatform/CheeseListingIsPublishedExtension.php
+++ src/ApiPlatform/CheeseListingIsPublishedExtension.php
@@ -3,24 +3,53 @@
 namespace App\ApiPlatform;
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\CheeseListing;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
 
-class CheeseListingIsPublishedExtension implements QueryCollectionExtensionInterface
+class CheeseListingIsPublishedExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security){
+        $this->security = $security;
+    }
+
+
+    /* note
+     * Oh, and by the way - in addition to $resourceClass, this method receives the $operationName.
+     * Normally only the get operation - like GET /api/cheese - would cause a query for a collection to be made...
+     * but if you created some custom operations and needed to tweak the query on an operation-by-operation basis,
+     * the $operationName can let you do that.
+     */
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        $this->addWhere($resourceClass, $queryBuilder);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        $this->addWhere($resourceClass, $queryBuilder);
+    }
+
+    /**
+     * @param string $resourceClass
+     * @param QueryBuilder $queryBuilder
+     */
+    public function addWhere(string $resourceClass, QueryBuilder $queryBuilder): void
     {
         if ($resourceClass !== CheeseListing::class) {
             return;
         }
 
-        /* note
-         * Oh, and by the way - in addition to $resourceClass, this method receives the $operationName.
-         * Normally only the get operation - like GET /api/cheese - would cause a query for a collection to be made...
-         * but if you created some custom operations and needed to tweak the query on an operation-by-operation basis,
-         * the $operationName can let you do that.
-         */
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return;
+        }
 
         $rootAlias = $queryBuilder->getRootAliases()[0];
         $queryBuilder->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
