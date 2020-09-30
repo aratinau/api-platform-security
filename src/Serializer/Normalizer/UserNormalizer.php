8070f6d37436297ba6d96a430dbd8e9f074ff266
Aymeric <hello@codeparadise.xyz>
2020-09-30 13:47:05 +0200

    A "Normalizer Aware" Normalizer

--- src/Serializer/Normalizer/UserNormalizer.php
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -4,17 +4,15 @@ namespace App\Serializer\Normalizer;
 
 use App\Entity\User;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 
-class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+class UserNormalizer implements ContextAwareNormalizerInterface, CacheableSupportsMethodInterface, NormalizerAwareInterface
 {
-    private $normalizer;
+    use NormalizerAwareTrait;
 
-    public function __construct(ObjectNormalizer $normalizer)
-    {
-        $this->normalizer = $normalizer;
-    }
+    private const ALREADY_CALLED = 'USER_NORMALIZER_ALREADY_CALLED';
 
     /**
      * @param User $object
@@ -24,18 +22,31 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
         if ($this->userIsOwner($object)) {
             $context['groups'][] = 'owner:read';
         }
+
+        $context[self::ALREADY_CALLED] = true;
+
         $data = $this->normalizer->normalize($object, $format, $context);
+
         // Here: add, edit, or delete some data
+
         return $data;
     }
-    public function supportsNormalization($data, $format = null): bool
+
+    public function supportsNormalization($data, $format = null, array $context = [])
     {
+        // avoid recursion: only call once per object
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
         return $data instanceof User;
     }
+
     private function userIsOwner(User $user): bool
     {
-        return true;
+        return mt_rand(0, 10) > 5;
     }
+
     public function hasCacheableSupportsMethod(): bool
     {
         return true;
